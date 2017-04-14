f9bfc9fa27810f3260394d506e02d47eab7cc015
Tang Kai Ling <kailing_813@hotmail.com>
2017-04-14 12:16:35 +0800

    Separated Exceptions into different files in Exceptions folder

---
+++ src/Exceptions/InvalidMoveException.java
@@ -0,0 +1,17 @@
+package Exceptions;
+
+public class InvalidMoveException extends Exception {
+	public InvalidMoveException(String...validPositions) {
+		super(getErrorMsg(validPositions));
+	}
+
+	private static String getErrorMsg(String... validPositions) {
+		String errorMsg = "That is not a valid move in this board! Valid board positions: \n";
+
+		for (String position : validPositions) {
+			errorMsg.concat("\t-" + position + "\n");
+		}
+
+		return errorMsg;
+	}
+}
