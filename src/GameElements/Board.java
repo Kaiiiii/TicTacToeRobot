01c5b39f78b5f683ca011224d9fb2e30c1b6c325
Tang Kai Ling <kailing_813@hotmail.com>
2017-04-16 21:30:26 +0800

    Renamed package, used const to define board size instead, added logic to find winner

--- src/GameElements/Board.java
+++ src/GameElements/Board.java
@@ -1,39 +1,100 @@
-package gameelements;
-
-import exceptions.InvalidMoveException;
+package GameElements;
 
 public class Board{
+	static final int BOARD_DIMENSIONS = 3;
+	
 	char[][] board;
 
 	public Board(){
-		this.board = new char[3][3];
+		this.board = new char[BOARD_DIMENSIONS][BOARD_DIMENSIONS];
 	}
 
 	public char[][] getGameState() {
 		return this.board;
 	}
 
-	public void move(char playerChar, int...position){
-		try{
-			if (position.length!=2){
-				throw new InvalidMoveException();
-			}
-
-			int row = position[0],
-			column = position[1];
-
-			if (row < 0 || row > 3 || column < 0 || column > 3){
-				throw new InvalidMoveException();
-			}
-
-			this.board[row][column] = playerChar;
-
-		} catch(Exception e){
-			System.out.println(e.getMessage());
-		}
+	public Board move(Player player, int row, int column){
+		this.board[row][column] = player.getChar();
+		return this;
 	}
-
-	private Player nextPlayer(){
-		return currPlayer.equals(firstPlayer) ? secondPlayer : firstPlayer;
+	
+	public Player gameWinner(Player player1, Player player2){
+		char horizontalWinner = horizontalMatches();
+		char verticalWinner = verticalMatches(); 
+		char diagonalWinner = diagonalMatches();
+		
+		char winnerChar = horizontalMatches();
+		if (winnerChar == Character.MIN_VALUE) winnerChar = verticalMatches();
+		
+		if (horizontalWinner != Character.MIN_VALUE) winnerChar = horizontalWinner;
+		else if (verticalWinner != Character.MIN_VALUE) winnerChar = verticalWinner;
+		else if (diagonalWinner != Character.MIN_VALUE) winnerChar = diagonalWinner;
+		
+		return null;
+	}
+	
+	private char horizontalMatches() {
+		for (int i = 0; i < BOARD_DIMENSIONS; i++ ) {
+			char firstRowChar = this.board[i][0];
+			
+			for (int j = 1; j < BOARD_DIMENSIONS; j++) {
+				if(firstRowChar != this.board[i][j]){
+					firstRowChar = Character.MIN_VALUE;
+					break;
+				}
+			}
+			
+			if (firstRowChar != Character.MIN_VALUE){
+				return firstRowChar;
+			}
+		}
+		
+		return Character.MIN_VALUE;
+	}
+	
+	private char verticalMatches() {
+		for (int i = 0; i < BOARD_DIMENSIONS; i++) {
+			char firstColChar = this.board[0][i]; 
+			
+			for (int j = 1; j < BOARD_DIMENSIONS; j++){
+				if (firstColChar != this.board[j][i]){
+					firstColChar = Character.MIN_VALUE;
+					break;
+				}
+			}
+			
+			if(firstColChar != Character.MIN_VALUE) {
+				return firstColChar;
+			}
+		}
+		return Character.MIN_VALUE;
+	}
+	
+	private char diagonalMatches() {
+		char diagChar = this.board[0][0];
+		for (int i = 1; i < BOARD_DIMENSIONS; i++) {
+			if (diagChar == this.board[i][i]){
+				diagChar = Character.MIN_VALUE;
+				break;
+			}
+		}
+		
+		if (diagChar != Character.MIN_VALUE){
+			return diagChar;
+		}
+		
+		diagChar = this.board[0][3];
+		for (int i = 1; i < BOARD_DIMENSIONS; i++){
+			if (diagChar == this.board[i][BOARD_DIMENSIONS-i-1]){
+				diagChar = Character.MIN_VALUE;
+				break;
+			}
+		}
+		
+		if (diagChar != Character.MIN_VALUE){
+			return diagChar;
+		}
+		
+		return Character.MIN_VALUE;
 	}
 }
