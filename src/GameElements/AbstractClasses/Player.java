02475964a969bfb2bb235113ab0b2687000b379f
Tang Kai Ling <kailing_813@hotmail.com>
2017-04-19 09:29:34 +0800

    Added weight to winning cases.

--- src/GameElements/AbstractClasses/Player.java
+++ src/GameElements/AbstractClasses/Player.java
@@ -6,17 +6,19 @@ public abstract class Player{
 	protected char playerChar;
 	protected int score;
 	
-	protected double w1_doubleOwnRow, 
-						w2_doubleEnemyRow, 
-						w3_interceptRow,
-						w4_interceptedRow;
+	protected int w1_doubleOwnRow, 
+					w2_doubleEnemyRow, 
+					w3_interceptRow,
+					w4_interceptedRow, 
+					w5_completedOwnRow, 
+					w6_completedEnemyRow; 
 	
 	public Player (char playerChar) {
 		this.playerChar = playerChar;
 		this.score = 0;
 	}
 	
-	public abstract double getScore(Board boardState);
+	public abstract int getScore(Board boardState);
 	public abstract int[] nextMove(Board boardState);
 
 	public char getChar() {
--- src/GameElements/DumbPlayer.java
+++ src/GameElements/DumbPlayer.java
@@ -17,14 +17,28 @@ public class DumbPlayer extends Player{
 	
 	public DumbPlayer(char playerChar){
 		super(playerChar); 
-		this.w1_doubleOwnRow = 0.2;
-		this.w2_doubleEnemyRow = 0.4;
-		this.w3_interceptRow = 0.2;
-		this.w4_interceptedRow = 0.4;
+		this.w1_doubleOwnRow = 20;
+		this.w2_doubleEnemyRow = -30;
+		this.w3_interceptRow = 60;
+		this.w4_interceptedRow = -30;
+		this.w5_completedOwnRow = 50;
+		this.w6_completedEnemyRow = -50;
 	}
 	
-	public double getScore(Board boardState){
+	public int getScore(Board boardState){
 		int[] boardAnalysis = analyzeBoard(boardState);
+		
+		if (boardAnalysis[4] > 0) return w5_completedOwnRow;
+		if (boardAnalysis[5] > 0) return w6_completedEnemyRow;
+		
+//		System.out.print("!!!doubleOwnRow " + boardAnalysis[0] + "\t");
+//		System.out.print("!!!doubleEnemyRow " + boardAnalysis[1]+ "\t");
+//		System.out.print("!!!interceptRow " + boardAnalysis[2]+ "\t");
+//		System.out.print("!!!interceptedRow " + boardAnalysis[3]+ "\t");
+//		System.out.print("!!!completedOwnRow " + boardAnalysis[4]+ "\t");
+//		System.out.print("!!!completedEnemyRow " + boardAnalysis[5]+ "\t");
+//		System.out.println();
+		
 		return w1_doubleOwnRow * boardAnalysis[0] + 
 				w2_doubleEnemyRow * boardAnalysis[1] + 
 				w3_interceptRow * boardAnalysis[2] + 
@@ -33,29 +47,25 @@ public class DumbPlayer extends Player{
 	
 	public int[] nextMove(Board boardState) {
 		int boardDimension = boardState.BOARD_DIMENSIONS;
-		double highestScore = 0;  
-		int[] posOfHighest = {0, 0};
-		double mockScore ;
+		int highestScore = Integer.MIN_VALUE, 
+			mockScore;  
+		int[] posOfHighest = {-1, -1};
 		
-		Board mockBoard = boardState.clone();
+		Board mockBoard;
 		
 		for (int i = 0; i < boardDimension; i++){
 			for (int j = 0; j < boardDimension; j++){
-				mockScore = 0;
-				if (!boardState.isPositionEmpty(i, j)) continue;
-				else {
-					try{
-						mockBoard.move(this, i, j);
-					}catch (SameMoveException sme){
-						continue;
-					}
-					mockScore = getScore(mockBoard); 
-					
-					if (mockScore > highestScore){
-						highestScore = mockScore; 
-						posOfHighest[0] = i;
-						posOfHighest[1] = j;
-					}
+				mockBoard = boardState.clone();
+				try{
+					mockBoard.move(this, i, j);
+				}catch (SameMoveException sme){
+					continue;
+				}
+				mockScore = getScore(mockBoard); 
+				if (mockScore > highestScore){
+					highestScore = mockScore; 
+					posOfHighest[0] = i;
+					posOfHighest[1] = j;
 				}
 			}
 		}
@@ -63,7 +73,7 @@ public class DumbPlayer extends Player{
 	}
 	
 	private int[] analyzeBoard(Board boardState){		
-		int[] boardScore = {0, 0, 0, 0};
+		int[] boardScore = {0, 0, 0, 0, 0, 0};
 		boardScore = analyzeHorizontal(boardState, boardScore);
 		boardScore = analyzeVertical(boardState, boardScore);
 		boardScore = analyzeDiagonal(boardState, boardScore);
@@ -84,7 +94,7 @@ public class DumbPlayer extends Player{
 			
 			for (int j = 0; j < boardDimensions; j++) {
 				if (board[i][j] == this.playerChar) ownCount ++; 
-				else enemyCount ++; 
+				else if (board[i][j] != Character.MIN_VALUE) enemyCount ++; 
 			}
 			
 			score = tallyRow(ownCount, enemyCount, score);
@@ -105,7 +115,7 @@ public class DumbPlayer extends Player{
 			
 			for (int j = 0; j < boardDimensions; j++){
 				if (board[j][i] == this.playerChar) ownCount ++; 
-				else enemyCount ++;
+				else if (board[j][i] != Character.MIN_VALUE) enemyCount ++;
 			}
 			
 			score = tallyRow(ownCount, enemyCount, score);
@@ -122,7 +132,7 @@ public class DumbPlayer extends Player{
 		
 		for (int i = 0; i < boardDimensions; i++){
 			if (board[i][i] == this.playerChar) ownCount ++;
-			else enemyCount ++;
+			else if (board[i][i] != Character.MIN_VALUE) enemyCount ++;
 		}
 		
 		score = tallyRow(ownCount, enemyCount, score);
@@ -131,7 +141,7 @@ public class DumbPlayer extends Player{
 		
 		for (int i = 0; i < boardDimensions; i++){
 			if (board[i][boardDimensions-i-1] == this.playerChar) ownCount ++;
-			else enemyCount ++;
+			else if (board[i][boardDimensions-i-1] != Character.MIN_VALUE) enemyCount ++;
 		}
 		
 		score = tallyRow(ownCount, enemyCount, score);
@@ -141,16 +151,16 @@ public class DumbPlayer extends Player{
 	private int[] tallyRow(int ownCount, int enemyCount, int[] currScore){
 		int[] score = currScore;
 		
-		if (ownCount >= 2){ 
+		if (ownCount == 2){ 
 			score[0] ++;
 			
-			if (enemyCount > 0) score[3] ++;
-		}
-		else if (enemyCount >= 2){
+			if (enemyCount == 1) score[3] ++;
+		} else if (ownCount == 3) score[4] ++;
+		else if (enemyCount == 2){
 			score[1] ++;
 			
-			if (ownCount > 0) score[2] ++;
-		}
+			if (ownCount == 1) score[2] ++;
+		} else if (enemyCount == 3) score[5] ++;
 		
 		return score;
 	}
