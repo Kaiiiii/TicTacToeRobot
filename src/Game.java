2b16a0b0671ed675bd79009ded313fbaed91a35d
Tang Kai Ling <kailing_813@hotmail.com>
2017-04-22 19:21:34 +0800

    Updated Game to use SmartPlayer + updated printing methods

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,9 @@
-import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 import Exceptions.SameMoveException;
 import GameElements.Board;
 import GameElements.DumbPlayer;
+import GameElements.SmartPlayer;
 import GameElements.AbstractClasses.Player;
 
 public class Game {
@@ -11,7 +11,28 @@ public class Game {
 		return currPlayer.equals(firstPlayer) ? secondPlayer : firstPlayer;
 	}
 	
+	private static Player playGame(Player firstPlayer, Player secondPlayer, Player currPlayer, Board board) throws Exception{
+		while(!board.gameEnded()){
+			try{
+				board = currPlayer.nextMove(board);				
+				currPlayer = nextPlayer(firstPlayer, secondPlayer, currPlayer);		
+				
+			} catch(SameMoveException sme){
+				System.out.println(sme.getMessage());
+			} catch(Exception e){
+				throw e;
+			}
+		}
+		
+//		printBoard(board.getGameState());
+		char winner = board.gameWinner();
+		if (winner == firstPlayer.getChar()) return firstPlayer;
+		if (winner == secondPlayer.getChar()) return secondPlayer; 
+		return null;
+	}
+	
 	private static void printBoard(char[][] board){
+		System.out.println("-----------");
 		for (int i = 0; i < board.length; i++){
 			System.out.print("|");
 			for (int j = 0; j < board[i].length; j++){
@@ -21,40 +42,46 @@ public class Game {
 			
 			System.out.println();
 		}
+		
+		System.out.println("-----------");
 	}
 	
 	public static void main(String[] args){
-		Board board = new Board();
+		Board board;
 		
-		Player firstPlayer = new DumbPlayer('o'); 
-		Player secondPlayer = new DumbPlayer('x'); 
+		int gamesWon = 0, 
+			draws = 0, 
+			totalGames = 100;
+		char myChar = 'x';
+		
+		Player firstPlayer = new SmartPlayer(myChar); 
+		Player secondPlayer = new DumbPlayer('o'); 
 		Player currPlayer = firstPlayer;
-		
-		Scanner input = new Scanner(System.in);
-		
-		while(!board.gameEnded()){
-			try{
-				System.out.println("Player " + currPlayer.getChar() + "'s turn!");
-				int[] nextMove = currPlayer.nextMove(board);
-				System.out.println(">> Player " + currPlayer.getChar() + " has chosen row " +  
-									nextMove[0] + " col " + nextMove[1] + ".");
-				
-				board.move(currPlayer, nextMove[0], nextMove[1]);
-				currPlayer = nextPlayer(firstPlayer, secondPlayer, currPlayer);
-				
-				printBoard(board.getGameState());
-//				TimeUnit.SECONDS.sleep(3);
-			} catch(SameMoveException sme){
-				System.out.println(sme.getMessage());
-			} catch(Exception e){
-				System.out.println(e.getMessage());
+		Player winner;
+
+		for (int i = 0; i < totalGames; i ++){
+			board = new Board();
+			winner = currPlayer;
+			try{ winner = playGame(firstPlayer, secondPlayer, currPlayer, board);}
+			catch(Exception e){
+				e.printStackTrace();
+				break;
+			}
+			
+			if (winner != null){
+				System.out.println("Player " + winner.getChar() + " wins!");
+				if (winner.getChar() == myChar){
+					gamesWon++;
+				}
+			}
+			else{				
+				System.out.println("It's a draw!");
+				draws++;
 			}
 		}
 		
-		System.out.print("\n>> ");
-		Player winner = board.gameWinner(firstPlayer, secondPlayer);
-		
-		if (winner != null) System.out.println(Character.toString(winner.getChar()) + " wins!");
-		else System.out.println("It's a draw!");
+		System.out.println(">> Total Games: " + totalGames);
+		System.out.println(">> Games Won: " + gamesWon);
+		System.out.println(">> Draws: " + draws);
 	}
 }
