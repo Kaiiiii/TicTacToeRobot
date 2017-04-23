77c7a7e3ce7708a893e04791ad64ed142f7b7d49
Tang Kai Ling <kailing_813@hotmail.com>
2017-04-23 20:05:04 +0800

    Added history logging for boardstates

--- src/GameElements/Board.java
+++ src/GameElements/Board.java
@@ -1,149 +1,170 @@
 package GameElements;
 
+import java.util.ArrayList;
+
+import Exceptions.InvalidMoveException;
 import Exceptions.SameMoveException;
 import GameElements.AbstractClasses.Player;
 
-public class Board{
+public class Board {
 	public static final int BOARD_DIMENSIONS = 3;
-	
-	char[][] board;
 
-	public Board(){
+	char[][] board;
+	ArrayList<Board> gameHistory;
+
+	public Board() {
 		this.board = new char[BOARD_DIMENSIONS][BOARD_DIMENSIONS];
+		this.gameHistory = new ArrayList<Board>();
 	}
-	
+
 	public Board clone() {
-	    Board newBoard = new Board();
-	    newBoard.board = copyBoard(this.board, BOARD_DIMENSIONS);
-	    
-	    return newBoard;
+		Board newBoard = new Board();
+		newBoard.board = copyBoard(this.board, BOARD_DIMENSIONS);
+
+		return newBoard;
 	}
 
 	public char[][] getGameState() {
 		return this.board;
 	}
 
-	public Board move(Player player, int row, int column) throws SameMoveException{
-		if (!isPositionEmpty(row, column)) throw new SameMoveException(this.board[row][column]);
-		
+	public Board move(Player player, int row, int column) throws SameMoveException, InvalidMoveException {
+		if (!isPositionEmpty(row, column))
+			throw new SameMoveException(this.board[row][column]);
+
+		if (row < 0 || row > BOARD_DIMENSIONS || column < 0 || column > BOARD_DIMENSIONS)
+			throw new InvalidMoveException();
+
 		this.board[row][column] = player.getChar();
+
+		this.gameHistory.add(this);
+
 		return this;
 	}
-	
-	public boolean isPositionEmpty(int row, int column){
-		if (this.board[row][column] != Character.MIN_VALUE)
-			return false;
-		return true;
-	}
-	
-	public char gameWinner(){
+
+	public char gameWinner() {
 		char winnerChar = Character.MIN_VALUE;
-		
-		if (!gameEnded()) return winnerChar;
-		
+
+		if (!gameEnded())
+			return winnerChar;
+
 		char horizontalWinner = horizontalMatches();
-		char verticalWinner = verticalMatches(); 
+		char verticalWinner = verticalMatches();
 		char diagonalWinner = diagonalMatches();
-		
-		if (horizontalWinner != Character.MIN_VALUE) winnerChar = horizontalWinner;
-		else if (verticalWinner != Character.MIN_VALUE) winnerChar = verticalWinner;
-		else if (diagonalWinner != Character.MIN_VALUE) winnerChar = diagonalWinner;
-			
+
+		if (horizontalWinner != Character.MIN_VALUE)
+			winnerChar = horizontalWinner;
+		else if (verticalWinner != Character.MIN_VALUE)
+			winnerChar = verticalWinner;
+		else if (diagonalWinner != Character.MIN_VALUE)
+			winnerChar = diagonalWinner;
+
 		return winnerChar;
 	}
-	
-	public boolean gameEnded(){
+
+	public boolean gameEnded() {
 		char horizontalWinner = horizontalMatches();
-		char verticalWinner = verticalMatches(); 
+		char verticalWinner = verticalMatches();
 		char diagonalWinner = diagonalMatches();
-		
-		if (horizontalWinner != Character.MIN_VALUE) return true;
-		if (verticalWinner != Character.MIN_VALUE) return true;
-		if (diagonalWinner != Character.MIN_VALUE) return true;
-		if (allPositionsFull()) return true;
-		
+
+		if (horizontalWinner != Character.MIN_VALUE)
+			return true;
+		if (verticalWinner != Character.MIN_VALUE)
+			return true;
+		if (diagonalWinner != Character.MIN_VALUE)
+			return true;
+		if (allPositionsFull())
+			return true;
+
 		return false;
 	}
-	
+
 	private char horizontalMatches() {
-		for (int i = 0; i < BOARD_DIMENSIONS; i++ ) {
+		for (int i = 0; i < BOARD_DIMENSIONS; i++) {
 			char firstRowChar = this.board[i][0];
-			
+
 			for (int j = 1; j < BOARD_DIMENSIONS; j++) {
-				if(firstRowChar != this.board[i][j]){
+				if (firstRowChar != this.board[i][j]) {
 					firstRowChar = Character.MIN_VALUE;
 					break;
 				}
 			}
-			
-			if (firstRowChar != Character.MIN_VALUE){
+
+			if (firstRowChar != Character.MIN_VALUE) {
 				return firstRowChar;
 			}
 		}
-		
+
 		return Character.MIN_VALUE;
 	}
-	
+
 	private char verticalMatches() {
 		for (int i = 0; i < BOARD_DIMENSIONS; i++) {
-			char firstColChar = this.board[0][i]; 
-			
-			for (int j = 1; j < BOARD_DIMENSIONS; j++){
-				if (firstColChar != this.board[j][i]){
+			char firstColChar = this.board[0][i];
+
+			for (int j = 1; j < BOARD_DIMENSIONS; j++) {
+				if (firstColChar != this.board[j][i]) {
 					firstColChar = Character.MIN_VALUE;
 					break;
 				}
 			}
-			
-			if(firstColChar != Character.MIN_VALUE) {
+
+			if (firstColChar != Character.MIN_VALUE) {
 				return firstColChar;
 			}
 		}
 		return Character.MIN_VALUE;
 	}
-	
+
 	private char diagonalMatches() {
 		char diagChar = this.board[0][0];
 		for (int i = 1; i < BOARD_DIMENSIONS; i++) {
-			if (diagChar != this.board[i][i]){
+			if (diagChar != this.board[i][i]) {
 				diagChar = Character.MIN_VALUE;
 				break;
 			}
 		}
-		
-		if (diagChar != Character.MIN_VALUE){
+
+		if (diagChar != Character.MIN_VALUE) {
 			return diagChar;
 		}
-		
+
 		diagChar = this.board[0][2];
-		for (int i = 1; i < BOARD_DIMENSIONS; i++){
-			if (diagChar != this.board[i][BOARD_DIMENSIONS-i-1]){
+		for (int i = 1; i < BOARD_DIMENSIONS; i++) {
+			if (diagChar != this.board[i][BOARD_DIMENSIONS - i - 1]) {
 				diagChar = Character.MIN_VALUE;
 				break;
 			}
 		}
-		
-		if (diagChar != Character.MIN_VALUE){
+
+		if (diagChar != Character.MIN_VALUE) {
 			return diagChar;
 		}
-		
+
 		return Character.MIN_VALUE;
 	}
 	
-	private boolean allPositionsFull(){
-		for (int i = 0; i < BOARD_DIMENSIONS; i++){
-			for (int j = 0; j < BOARD_DIMENSIONS; j++){
-				if (this.board[i][j] == Character.MIN_VALUE) return false;
+	private boolean isPositionEmpty(int row, int column) {
+		if (this.board[row][column] != Character.MIN_VALUE)
+			return false;
+		return true;
+	}
+
+	private boolean allPositionsFull() {
+		for (int i = 0; i < BOARD_DIMENSIONS; i++) {
+			for (int j = 0; j < BOARD_DIMENSIONS; j++) {
+				if (this.board[i][j] == Character.MIN_VALUE)
+					return false;
 			}
 		}
 		return true;
 	}
-	
-	private char[][] copyBoard(char[][] original, int dimension){
+
+	private char[][] copyBoard(char[][] original, int dimension) {
 		char[][] copy = new char[dimension][dimension];
-		
-		for (int i = 0; i < dimension; i++){
-			for (int j = 0; j < dimension; j++){
+
+		for (int i = 0; i < dimension; i++) {
+			for (int j = 0; j < dimension; j++) {
 				copy[i][j] = original[i][j];
 			}
 		}
