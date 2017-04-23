27c1553d5a2fd6465008a0f9da289474c87c8b18
Tang Kai Ling <kailing_813@hotmail.com>
2017-04-24 00:15:22 +0800

    Changed weights for DumbPlayer

--- src/GameElements/DumbPlayer.java
+++ src/GameElements/DumbPlayer.java
@@ -1,10 +1,9 @@
 package GameElements;
 
-import Exceptions.SameMoveException;
 import GameElements.AbstractClasses.Player;
 
-public class DumbPlayer extends Player{
-	public DumbPlayer(char playerChar){
-		super(playerChar, 20, -30, 60, -30, 100, -100); 
+public class DumbPlayer extends Player {
+	public DumbPlayer(char playerChar) {
+		super(playerChar, 0.5, -0.5, 0.5, -0.5, 0.5, -0.5);
 	}
 }
