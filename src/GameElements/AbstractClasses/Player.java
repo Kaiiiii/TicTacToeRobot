a4f70234dc891841cd2a89374564dbc6a92c4e63
Tang Kai Ling <kailing_813@hotmail.com>
2017-04-24 00:18:25 +0800

    Implemented SmartPlayer with some reference to http://www.christopia.net/media/redactor/TicTacToe.py
    What I did wrong:
    	- actual value differs from estimated value of board:
    		* for actual value (Vtrain), board score towards end of game is equal to points given for winning/losing
    		* for estimated value (Vhat), board score at any point of game is calculated by estimated equation
    	- weights SHOULD NOT be updated every round, but after every game
    
    ++ formatting in Player.java

--- src/GameElements/AbstractClasses/Player.java
+++ src/GameElements/AbstractClasses/Player.java
@@ -4,20 +4,13 @@ import Exceptions.InvalidMoveException;
 import Exceptions.SameMoveException;
 import GameElements.Board;
 
-public abstract class Player{
-	protected static final double WINNING_SCORE = 100,
-								LOSING_SCORE = -100,
-								DRAW_SCORE = 0;
-	protected char playerChar;
+public abstract class Player {
+	protected final char playerChar;
 
-	protected double w1_doubleOwnRow,
-					w2_doubleEnemyRow,
-					w3_ownFreeRow,
-					w4_enemyFreeRow,
-					w5_interceptEnemyRow,
-					w6_interceptedOwnRow;
+	protected double w1_doubleOwnRow, w2_doubleEnemyRow, w3_ownFreeRow, w4_enemyFreeRow, w5_interceptEnemyRow,
+			w6_interceptedOwnRow;
 
-	public Player (char playerChar, double w1, double w2, double w3, int w4, int w5, int w6) {
+	public Player(char playerChar, double w1, double w2, double w3, double w4, double w5, double w6) {
 		this.playerChar = playerChar;
 
 		this.w1_doubleOwnRow = w1;
@@ -32,34 +25,27 @@ public abstract class Player{
 		return this.playerChar;
 	}
 
-
-	public Board nextMove(Board boardState) throws SameMoveException, InvalidMoveException{
+	public Board nextMove(Board boardState) throws SameMoveException, InvalidMoveException {
 		int[] nextMove = findBestMove(boardState, this);
-		int boardDimension = boardState.BOARD_DIMENSIONS;
-
-		if (nextMove[0] < 0 || nextMove[0] > boardDimension || nextMove[1] < 0 || nextMove[1] > boardDimension)
-			throw new InvalidMoveException();
-
 		return boardState.move(this, nextMove[0], nextMove[1]);
 	}
 
-	protected int[] findBestMove(Board boardState, Player player){
-		int boardDimension = boardState.BOARD_DIMENSIONS;
-		double highestScore = -Double.MAX_VALUE,
-			mockScore;
-		int[] posOfHighest = {-1, -1};
+	protected int[] findBestMove(Board boardState, Player player) {
+		int boardDimension = Board.BOARD_DIMENSIONS;
+		double highestScore = -Double.MAX_VALUE, mockScore;
+		int[] posOfHighest = { -1, -1 };
 
 		Board mockBoard;
-		for (int i = 0; i < boardDimension; i++){
-			for (int j = 0; j < boardDimension; j++){
+		for (int i = 0; i < boardDimension; i++) {
+			for (int j = 0; j < boardDimension; j++) {
 				mockBoard = boardState.clone();
-				try{
+				try {
 					mockBoard.move(this, i, j);
-				}catch (SameMoveException sme){
+				} catch (Exception e) {
 					continue;
 				}
-				mockScore = getScore(mockBoard);
-				if (mockScore > highestScore){
+				mockScore = getPredictedValue(mockBoard);
+				if (mockScore > highestScore) {
 					highestScore = mockScore;
 					posOfHighest[0] = i;
 					posOfHighest[1] = j;
@@ -70,28 +56,16 @@ public abstract class Player{
 		return posOfHighest;
 	}
 
-	protected double getScore(Board boardState){
+	protected double getPredictedValue(Board boardState) {
 		int[] boardAnalysis = analyzeBoard(boardState);
 
-		if (boardState.gameEnded()){
-			if (boardState.gameWinner() == this.playerChar){
-				return WINNING_SCORE;
-			} else if (boardState.gameWinner() == Character.MIN_VALUE){
-				return DRAW_SCORE;
-			}
-			return LOSING_SCORE;
-		}
-
-		return this.w1_doubleOwnRow * boardAnalysis[0] +
-				this.w2_doubleEnemyRow * boardAnalysis[1] +
-				this.w3_ownFreeRow * boardAnalysis[2] +
-				this.w4_enemyFreeRow * boardAnalysis[3] +
-				this.w5_interceptEnemyRow * boardAnalysis[4] +
-				this.w6_interceptedOwnRow * boardAnalysis[5];
+		return this.w1_doubleOwnRow * boardAnalysis[0] + this.w2_doubleEnemyRow * boardAnalysis[1]
+				+ this.w3_ownFreeRow * boardAnalysis[2] + this.w4_enemyFreeRow * boardAnalysis[3]
+				+ this.w5_interceptEnemyRow * boardAnalysis[4] + this.w6_interceptedOwnRow * boardAnalysis[5];
 	}
 
-	protected int[] analyzeBoard(Board boardState){
-		int[] boardScore = {0, 0, 0, 0, 0, 0};
+	protected int[] analyzeBoard(Board boardState) {
+		int[] boardScore = { 0, 0, 0, 0, 0, 0 };
 		boardScore = analyzeHorizontal(boardState, boardScore);
 		boardScore = analyzeVertical(boardState, boardScore);
 		boardScore = analyzeDiagonal(boardState, boardScore);
@@ -99,36 +73,9 @@ public abstract class Player{
 		return boardScore;
 	}
 
-	private int[] analyzeHorizontal(Board boardState, int[] boardScore){
+	private int[] analyzeHorizontal(Board boardState, int[] boardScore) {
 		int[] score = boardScore;
-		int boardDimensions = Board.BOARD_DIMENSIONS,
-			ownCount,
-			enemyCount,
-			emptyCount;
-		char[][] board = boardState.getGameState();
-
-		for (int i = 0; i < boardDimensions; i++ ) {
-			ownCount = 0;
-			enemyCount = 0;
-			emptyCount = 0;
-
-			for (int j = 0; j < boardDimensions; j++) {
-				if (board[i][j] == this.playerChar) ownCount ++;
-				else if (board[i][j] == Character.MIN_VALUE) emptyCount ++;
-				else enemyCount++;
-			}
-
-			score = tallyRow(ownCount, enemyCount, emptyCount,score);
-		}
-		return score;
-	}
-
-	private int[] analyzeVertical(Board boardState, int[] boardScore){
-		int[] score = boardScore;
-		int boardDimensions = Board.BOARD_DIMENSIONS,
-				ownCount,
-				enemyCount,
-				emptyCount;
+		int boardDimensions = Board.BOARD_DIMENSIONS, ownCount, enemyCount, emptyCount;
 		char[][] board = boardState.getGameState();
 
 		for (int i = 0; i < boardDimensions; i++) {
@@ -136,10 +83,13 @@ public abstract class Player{
 			enemyCount = 0;
 			emptyCount = 0;
 
-			for (int j = 0; j < boardDimensions; j++){
-				if (board[j][i] == this.playerChar) ownCount ++;
-				else if (board[j][i] == Character.MIN_VALUE) emptyCount ++;
-				else enemyCount ++;
+			for (int j = 0; j < boardDimensions; j++) {
+				if (board[i][j] == this.playerChar)
+					ownCount++;
+				else if (board[i][j] == Character.MIN_VALUE)
+					emptyCount++;
+				else
+					enemyCount++;
 			}
 
 			score = tallyRow(ownCount, enemyCount, emptyCount, score);
@@ -147,18 +97,42 @@ public abstract class Player{
 		return score;
 	}
 
-	private int[] analyzeDiagonal(Board boardState, int[] boardScore){
+	private int[] analyzeVertical(Board boardState, int[] boardScore) {
 		int[] score = boardScore;
-		int boardDimensions = Board.BOARD_DIMENSIONS,
-				ownCount = 0,
-				enemyCount = 0,
-				emptyCount = 0;
+		int boardDimensions = Board.BOARD_DIMENSIONS, ownCount, enemyCount, emptyCount;
 		char[][] board = boardState.getGameState();
 
-		for (int i = 0; i < boardDimensions; i++){
-			if (board[i][i] == this.playerChar) ownCount ++;
-			else if (board[i][i] == Character.MIN_VALUE) emptyCount ++;
-			else enemyCount ++;
+		for (int i = 0; i < boardDimensions; i++) {
+			ownCount = 0;
+			enemyCount = 0;
+			emptyCount = 0;
+
+			for (int j = 0; j < boardDimensions; j++) {
+				if (board[j][i] == this.playerChar)
+					ownCount++;
+				else if (board[j][i] == Character.MIN_VALUE)
+					emptyCount++;
+				else
+					enemyCount++;
+			}
+
+			score = tallyRow(ownCount, enemyCount, emptyCount, score);
+		}
+		return score;
+	}
+
+	private int[] analyzeDiagonal(Board boardState, int[] boardScore) {
+		int[] score = boardScore;
+		int boardDimensions = Board.BOARD_DIMENSIONS, ownCount = 0, enemyCount = 0, emptyCount = 0;
+		char[][] board = boardState.getGameState();
+
+		for (int i = 0; i < boardDimensions; i++) {
+			if (board[i][i] == this.playerChar)
+				ownCount++;
+			else if (board[i][i] == Character.MIN_VALUE)
+				emptyCount++;
+			else
+				enemyCount++;
 		}
 
 		score = tallyRow(ownCount, enemyCount, emptyCount, score);
@@ -166,32 +140,41 @@ public abstract class Player{
 		enemyCount = 0;
 		emptyCount = 0;
 
-		for (int i = 0; i < boardDimensions; i++){
-			if (board[i][boardDimensions-i-1] == this.playerChar) ownCount ++;
-			else if (board[i][boardDimensions-i-1] == Character.MIN_VALUE) emptyCount ++;
-			else enemyCount ++;
+		for (int i = 0; i < boardDimensions; i++) {
+			if (board[i][boardDimensions - i - 1] == this.playerChar)
+				ownCount++;
+			else if (board[i][boardDimensions - i - 1] == Character.MIN_VALUE)
+				emptyCount++;
+			else
+				enemyCount++;
 		}
 
 		score = tallyRow(ownCount, enemyCount, emptyCount, score);
 		return score;
 	}
 
-	private int[] tallyRow(int ownCount, int enemyCount, int emptyCount, int[] currScore){
+	private int[] tallyRow(int ownCount, int enemyCount, int emptyCount, int[] currScore) {
 		int[] score = currScore;
-		if(ownCount == 1){
-			if (emptyCount == 2) score[2] ++;
-		} else if (ownCount == 2){
-			score[0] ++;
-			if (emptyCount == 1) score[2] ++;
-			else if (enemyCount == 1) score[5] ++;
+		if (ownCount == 1) {
+			if (emptyCount == 2)
+				score[2]++;
+		} else if (ownCount == 2) {
+			score[0]++;
+			if (emptyCount == 1)
+				score[2]++;
+			else if (enemyCount == 1)
+				score[5]++;
 		}
 
-		if (enemyCount == 1){
-			if (emptyCount == 2) score[3] ++;
-		} else if (enemyCount == 2){
-			score[1] ++;
-			if (emptyCount == 1) score[3] ++;
-			else if (ownCount == 1) score[4] ++;
+		if (enemyCount == 1) {
+			if (emptyCount == 2)
+				score[3]++;
+		} else if (enemyCount == 2) {
+			score[1]++;
+			if (emptyCount == 1)
+				score[3]++;
+			else if (ownCount == 1)
+				score[4]++;
 		}
 
 		return score;
--- src/GameElements/SmartPlayer.java
+++ src/GameElements/SmartPlayer.java
@@ -1,81 +1,99 @@
 package GameElements;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 import Exceptions.InvalidMoveException;
 import Exceptions.SameMoveException;
-import GameElements.AbstractClasses.Player;
 
-public class SmartPlayer extends DumbPlayer{
-	private final double modifier = 0.1;
-					
-	public SmartPlayer (char playerChar){
+public class SmartPlayer extends DumbPlayer {
+	private static final double WINNING_SCORE = 100, LOSING_SCORE = -100, DRAW_SCORE = 0;
+
+	private final double modifier = 0.4;
+
+	public SmartPlayer(char playerChar) {
 		super(playerChar);
 	}
-	
-	public Board nextMove(Board boardState) throws SameMoveException, InvalidMoveException{
-		char enemyChar = this.playerChar == 'x' ? 'o' : 'x';
-		double currentValue = getScore(boardState);
-		
+
+	public Board nextMove(Board boardState) throws SameMoveException, InvalidMoveException {
 		Board nextBoard = super.nextMove(boardState);
-		
-		double successorValue = predictSuccessorValue(nextBoard, enemyChar);
-		updateWeights(successorValue, currentValue, boardState);
-		
-//		System.out.println("double own row " + w1_doubleOwnRow);
-//		System.out.println("double enemy row " + w2_doubleEnemyRow);
-//		System.out.println("own free row " + w3_ownFreeRow);
-//		System.out.println("enemy free row " + w4_enemyFreeRow);
-//		System.out.println("intercept enemy row " + w5_interceptEnemyRow);
-//		System.out.println("intercepted own row " + w6_interceptedOwnRow);
-//		System.out.println("completed own row " + w7_completedOwnRow);
-//		System.out.println("completed enemy row " + w8_completedEnemyRow);
-		
-		return nextBoard;	
+
+		return nextBoard;
 	}
 
-	private double predictSuccessorValue(Board boardState, char enemyChar){
-		Board mockBoard = boardState.clone();
+	public void updateWeights(ArrayList<Board> history){
+		// Vtrain: get training value of board --> actual value 
+		// Vboard: get score of current board
 		
-		Player mockPlayer = new DumbPlayer(enemyChar);
+		Queue<Double> trainingValues = this.getActualBoardValue(history);
+		double error;
+		int[] boardStateAnalysis;
+		Board currBoard;
 		
-		try{ mockPlayer.nextMove(mockBoard); } 
-		catch(SameMoveException sme){ return 0; }
-		catch(InvalidMoveException ime){ return 0; }
-		
-//		System.out.println("curr board");
-//		printBoard(boardState.getGameState());
-//		System.out.println("Mock board");
-//		printBoard(mockBoard.getGameState());
-		return this.getScore(mockBoard);
+		for (int i = 0; i < history.size(); i += 2){
+			currBoard = history.get(i);
+			boardStateAnalysis = this.analyzeBoard(currBoard);
+			error = trainingValues.remove() - this.getPredictedValue(currBoard);
+			
+			this.w1_doubleOwnRow += modifier * error * boardStateAnalysis[0];
+			this.w2_doubleEnemyRow += modifier * error * boardStateAnalysis[1]; 
+			this.w3_ownFreeRow += modifier * error * boardStateAnalysis[2];
+			this.w4_enemyFreeRow += modifier * error * boardStateAnalysis[3];
+			this.w5_interceptEnemyRow += modifier * error * boardStateAnalysis[4];
+			this.w6_interceptedOwnRow += modifier * error * boardStateAnalysis[5];
+		}
 	}
 	
-	public void updateWeights(double trainingValue, double currentScore, Board boardState){
-		double blah = this.getScore(boardState);
-		double error = trainingValue - blah;
-//		System.out.println("training value " + trainingValue);
-//		System.out.println("new stuff " + blah);
-
-		int[] boardStateAnalysis = analyzeBoard(boardState);
+	private Queue<Double> getActualBoardValue(ArrayList<Board> history){
+		int historyCount = history.size();
+		Queue<Double> historyScores = new LinkedList<Double>();
 		
-		this.w1_doubleOwnRow += modifier * error * boardStateAnalysis[0];
-		this.w2_doubleEnemyRow += modifier * error * boardStateAnalysis[1]; 
-		this.w3_ownFreeRow += modifier * error * boardStateAnalysis[2];
-		this.w4_enemyFreeRow += modifier * error * boardStateAnalysis[3];
-		this.w5_interceptEnemyRow += modifier * error * boardStateAnalysis[4];
-		this.w6_interceptedOwnRow += modifier * error * boardStateAnalysis[5];
-	}
-	
-	private void printBoard(char[][] board){
-		System.out.println("-----------");
-		for (int i = 0; i < board.length; i++){
-			System.out.print("|");
-			for (int j = 0; j < board[i].length; j++){
-				if (board[i][j] == Character.MIN_VALUE) System.out.print(" |");
-				else System.out.print(Character.toString(board[i][j]) + '|'); 
+		for (int i = 0; i < historyCount; i += 2){			
+			if (i + 2 >= historyCount - 1){
+				/*
+				 * This condition reflects the case where the next entry in the game history
+				 * is the last state (i.e. the opponent's move has ended the game). 
+				 */
+				if (i + 1 == historyCount - 1 && history.get(i + 1).gameEnded()){				
+					char winnerChar = history.get(i + 1).gameWinner();
+					if (winnerChar == Character.MIN_VALUE){
+						historyScores.add(DRAW_SCORE);
+						continue;
+					}
+					historyScores.add(LOSING_SCORE);
+					continue;
+				}
+				
+				/*
+				 * The following two cases are caught in this condition: 
+				 * 	1. The successor state is the end of the game.
+				 * 	2. The current state is the end of the game.
+				 */
+				Board currBoard = history.get(i);
+				
+				if (i + 2 == historyCount - 1){
+					currBoard = history.get(i + 2);
+				}
+				
+				char winnerChar = currBoard.gameWinner();
+				
+				if(winnerChar == this.playerChar){
+					historyScores.add(WINNING_SCORE);
+					continue;
+				} 
+				if (winnerChar == Character.MIN_VALUE){
+					historyScores.add(DRAW_SCORE);
+					continue;
+				}
+				historyScores.add(LOSING_SCORE);
+				continue;
 			}
 			
-			System.out.println();
+			historyScores.add(this.getPredictedValue(history.get(i)));
 		}
 		
-		System.out.println("-----------");
+		return historyScores;
 	}
+	
 }
