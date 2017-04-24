6e6bba9dbbb53694657a15fb0b052a1818c98cad
Tang Kai Ling <kailing_813@hotmail.com>
2017-04-24 13:22:17 +0800

    Changed default weights in DumbPlayer

--- src/GameElements/DumbPlayer.java
+++ src/GameElements/DumbPlayer.java
@@ -4,6 +4,6 @@ import GameElements.AbstractClasses.Player;
 
 public class DumbPlayer extends Player {
 	public DumbPlayer(char playerChar) {
-		super(playerChar, 0.5, -0.5, 0.5, -0.5, 0.5, -0.5);
+		super(playerChar, 0.2, -0.3, 0.6, -0.2, 0.1, -0.4);
 	}
 }
